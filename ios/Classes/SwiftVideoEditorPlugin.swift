fa05756d3ec35f2a25882cb1868f64850e0890ca
Lewo <levent.ozkan@radity.com>
2021-10-12 19:32:47 +0300

    add get media info Ios done

--- ios/Classes/SwiftVideoEditorPlugin.swift
+++ ios/Classes/SwiftVideoEditorPlugin.swift
@@ -1,44 +1,100 @@
 import Flutter
 import UIKit
+import AVFoundation
 
 public class SwiftVideoEditorPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "video_editor", binaryMessenger: registrar.messenger())
-    let instance = SwiftVideoEditorPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "writeVideofile":
-        let video = VideoGeneratorService()
-        guard let args = call.arguments as? [String: Any] else {
-          result(FlutterError(code: "arguments_not_found",
-                            message: "the arguments is not found.",
-                            details: nil))
-          return
-        }
-        guard let srcName = args["srcFilePath"] as? String else {
-                result(FlutterError(code: "src_file_path_not_found",
-                                  message: "the src file path sr is not found.",
-                                  details: nil))
-                return
-        }
-        guard let destName = args["destFilePath"] as? String else {
-                result(FlutterError(code: "dest_file_path_not_found",
-                                  message: "the dest file path is not found.",
-                                  details: nil))
-                return
-        }
-        guard let processing = args["processing"] as?  [String: [String: Any]] else {
-                result(FlutterError(code: "processing_data_not_found",
-                                  message: "the processing is not found.",
-                                  details: nil))
-                return
-        }
-        video.writeVideofile(srcPath: srcName, destPath: destName, processing: processing,result: result)
-    default:
-        result("iOS d" + UIDevice.current.systemVersion)
+    
+    private let avController = AvController()
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "video_editor", binaryMessenger: registrar.messenger())
+        let instance = SwiftVideoEditorPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        
+        guard let args = call.arguments as? [String: Any] else {
+            result(FlutterError(code: "arguments_not_found",
+                                message: "the arguments is not found.",
+                                details: nil))
+            return
+        }
+        
+        switch call.method {
+        case "writeVideofile":
+            let video = VideoGeneratorService()
+            
+            guard let srcName = args["srcFilePath"] as? String else {
+                result(FlutterError(code: "src_file_path_not_found",
+                                    message: "the src file path sr is not found.",
+                                    details: nil))
+                return
+            }
+            guard let destName = args["destFilePath"] as? String else {
+                result(FlutterError(code: "dest_file_path_not_found",
+                                    message: "the dest file path is not found.",
+                                    details: nil))
+                return
+            }
+            guard let processing = args["processing"] as?  [String: [String: Any]] else {
+                result(FlutterError(code: "processing_data_not_found",
+                                    message: "the processing is not found.",
+                                    details: nil))
+                return
+            }
+            video.writeVideofile(srcPath: srcName, destPath: destName, processing: processing,result: result)
+        case "getMediaInfo":
+            guard let srcName = args["srcFilePath"] as? String else {
+                result(FlutterError(code: "src_file_path_not_found",
+                                    message: "the src file path sr is not found.",
+                                    details: nil))
+                return
+            }
+            let json = getMediaInfo(path: srcName)
+            let string = Utility.keyValueToJson(json)
+            
+            result(string)
+            
+        default:
+            result("iOS d" + UIDevice.current.systemVersion)
+        }
+    }
+    
+    private func getMediaInfo(path:String) -> [String : Any?]{
+        let url = Utility.getPathUrl(path)
+        let asset = avController.getVideoAsset(url)
+        guard let track = avController.getTrack(asset) else { return [:] }
+        
+        let playerItem = AVPlayerItem(url: url)
+        let metadataAsset = playerItem.asset
+        
+        
+        let title = avController.getMetaDataByTag(metadataAsset,key: "title")
+        let author = avController.getMetaDataByTag(metadataAsset,key: "author")
+        
+        let duration = asset.duration.seconds * 1000
+        let filesize = track.totalSampleDataLength
+        
+        let size = track.naturalSize.applying(track.preferredTransform)
+        
+        let aspectRatio = size.width / size.height
+        
+        let width = abs(size.width)
+        let height = abs(size.height)
+        
+        let dictionary = [
+            "path":Utility.excludeFileProtocol(path),
+            "title":title,
+            "author":author,
+            "width":width,
+            "height":height,
+            "duration":duration,
+            "filesize":filesize,
+            "aspectratio":aspectRatio,
+        ] as [String : Any?]
+        
+        return dictionary
+        
+    }
 }
